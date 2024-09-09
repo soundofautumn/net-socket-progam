581d16a6a342511b866e9690e8e9295eb2a6d5bc
soundofautumn <qjm1412@163.com>
2024-09-09 19:15:05 +0800

    feat: 优化

--- client/src/DefaultClientProcessor.java
+++ client/src/DefaultClientProcessor.java
@@ -8,8 +8,6 @@ public class DefaultClientProcessor implements ClientProcessor {
 
     private final Scanner scanner = new Scanner(System.in);
 
-    private String username;
-
     @Override
     public void showUsage() {
         System.out.println("Usage:");
@@ -18,36 +16,40 @@ public class DefaultClientProcessor implements ClientProcessor {
         System.out.println("echo <username> <message>");
     }
 
+    private String processCommand(String commandStr) {
+        final String[] commandStrs = commandStr.split(" ");
+        if (commandStr.startsWith("login")) {
+            if (commandStrs.length != 3) {
+                System.out.println("Usage: login <username> <password>");
+                return null;
+            }
+            return "login " + commandStrs[1] + " " + commandStrs[2];
+        } else if (commandStr.startsWith("register")) {
+            if (commandStrs.length != 3) {
+                System.out.println("Usage: register <username> <password>");
+                return null;
+            }
+            return "register " + commandStrs[1] + " " + commandStrs[2];
+        } else if (commandStr.startsWith("echo")) {
+            if (commandStrs.length < 3) {
+                System.out.println("Usage: echo <username> <message>");
+                return null;
+            }
+            return "echo " + commandStrs[1] + " " + commandStrs[2];
+        }
+        return null;
+    }
+
     @Override
     public String send() {
         while (true) {
+            System.out.print(">");
             final String commandStr = scanner.nextLine();
-            if (commandStr.equals("exit")) {
-                return null;
-            }
-            final String[] commandStrs = commandStr.split(" ");
-            if (commandStr.startsWith("login")) {
-                if (commandStrs.length != 3) {
-                    System.out.println("Usage: login <username> <password>");
-                    continue;
-                }
-                return "login " + commandStrs[1] + " " + commandStrs[2];
-            } else if (commandStr.startsWith("register")) {
-                if (commandStrs.length != 3) {
-                    System.out.println("Usage: register <username> <password>");
-                    continue;
-                }
-                return "register " + commandStrs[1] + " " + commandStrs[2];
-            } else if (commandStr.startsWith("echo")) {
-                if (commandStrs.length < 3) {
-                    System.out.println("Usage: echo <username> <message>");
-                    continue;
-                }
-                return "echo " + commandStrs[1] + " " + commandStrs[2];
+            final String result = processCommand(commandStr);
+            if (result != null) {
+                return result;
             }
         }
-
-
     }
 
     @Override
